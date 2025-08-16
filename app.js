5bc2e9c7127484ecedcf5c33102fb22bacca123c
Yasin <yasin.enis.tr@gmail.com>
2025-08-16 19:44:24 +0300

    ✨ static files uploaded

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// MIDDLEWARES
+app.use(express.static('public'));
+
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server started at ${port} port`);
+  console.log(`Server started at ${port} port..`);
 });
